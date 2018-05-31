44dfdc7abbfc961e1d41b225722795ac2b50f4b1
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-05-31 12:47:39 -0700

    Adding ES2015 exercises and changing mock3

---
+++ ES2015/part-one/ES2015-exercise.js
@@ -0,0 +1,23 @@
+// ES5:
+// var name = 'Michael';
+// var favoriteColor = 'purple';
+// var city = 'San Francisco';
+
+// console.log(
+//   name +
+//     ' lives in ' +
+//     city +
+//     ' and his favorite color is ' +
+//     favoriteColor +
+//     '.'
+// );
+
+// ES6:
+const name = 'Michael';
+const favoriteColor = 'purple';
+const city = 'San Francisco';
+
+console.log(
+  `${name} lives in ${city} and his favorite color is ${favoriteColor}.`
+);
+
