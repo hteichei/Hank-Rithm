99dfc51ec0c75bfc3c334ecbdf66b909ecc850d9
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-06-04 19:10:25 -0700

    Committing ajax_exercises

---
+++ ajax_exercises/ajax-practice.js
@@ -0,0 +1,33 @@
+// $(document).ready(function() {
+//   $('#generate').on('click', function(event) {
+//     event.preventDefault();
+//     $.get(
+//       'http://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=fajNPYoDN4oQpgAES88YyPaON6EeEa5d',
+//       function(data) {
+//         console.log(data);
+//         var gif = $('<img>', {
+//           src: data.data[0].images.fixed_height.url
+//         });
+//         $('#gifContainer').append(gif);
+//       }
+//     );
+//   });
+// });
+
+$(document).ready(function() {
+  $('#generate').on('click', function(event) {
+    event.preventDefault();
+    $.get(
+      `http://api.giphy.com/v1/gifs/search?q=${$(
+        '#search'
+      ).val()}&rating=g&api_key=fajNPYoDN4oQpgAES88YyPaON6EeEa5d`,
+      function(data) {
+        console.log(data);
+        var gif = $('<img>', {
+          src: data.data[0].images.fixed_height.url
+        });
+        $('#gifContainer').append(gif);
+      }
+    );
+  });
+});
